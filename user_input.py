adc4b385fbea8b7b48c0781475e87cb4a96ba565
unknown <kariukibenard189@gmail.com>
2024-02-22 10:01:58 +0300

    Week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#Take input from a user, name ,age ,location.
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+#print user input
+print("Your name is", name ,"you are",age ,"years old and you come from",location, end=".")
