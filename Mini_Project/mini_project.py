b56775fdf089cefb8349496b1d8383a52c4929b1
unknown <kariukibenard189@gmail.com>
2024-04-17 09:06:06 +0300

    Made some changes.

--- Mini_Project/mini_project.py
+++ Mini_Project/mini_project.py
@@ -1,16 +1,16 @@
 import json
 
-file = open("C:\\Users\\kariu\\OneDrive\\Desktop\\PLP\\Mini_Project\\data.json","r")
-dictionary_data =json.load(file)
+with open("C:\\Users\\kariu\\OneDrive\\Desktop\\PLP\\Mini_Project\\data.json","r") as file:
+    dictionary_data =json.load(file)
 
-def look_word(word):
-    if word in dictionary_data:
+    def look_word(word):
+      if word in dictionary_data:
         return dictionary_data[word]
-    else:
+      else:
         print("Data not found.")
 
-input_word = input("Enter word: ")  
+    input_word = input("Enter word: ")  
 
-explanation = look_word(input_word) 
+    explanation = look_word(input_word) 
 
-print(explanation)     
+    print(explanation)     
